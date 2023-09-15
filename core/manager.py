89d9ce538a14a079b2c8108885e5561dfae1ea52
kwalpratham <kwalpratham>
2023-09-15 16:11:19 +0530

    fix: refactoring of code

--- core/manager.py
+++ core/manager.py
@@ -1,11 +1,19 @@
 # # todo : use base64 for create short url
-
-
 import string
+from flask import request
 from random import choice
+from core import app, db
+from core.models import ShortUrls
 
 
 class Manager:
-    def generate_short_id(self):
+    def generate_short_id(self, ):
         """Function to generate short_id of 8 of characters"""
         return ''.join(choice(string.ascii_letters+string.digits) for _ in range(8))
+    
+    def add_newurl(self, new_url: ShortUrls):
+        db.session.add(new_url)
+        db.session.commit()
+        hits = new_url.hits
+        short_url = request.host_url + new_url.short_id
+        return [short_url, hits]
--- core/routes.py
+++ core/routes.py
@@ -11,24 +11,39 @@ def index():
     if request.method == 'POST':
         url = request.form['url']
         short_id = request.form['custom_id']
+        
 
-        # In case of null entry
+        # Null entry
         if not url:
             flash('The URL is required!')
             return redirect(url_for('index'))
 
 
+        # Custom id already present in database
         elif short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
             flash('Please enter different custom id!')
             return redirect(url_for('index'))
-        
+    
+
         # known url
         url_object = ShortUrls.query.filter_by(original_url=url).first()
         if url_object is not None:
+            if short_id and short_id != url_object.short_id:
+                flash('Short id already exists, can not be changed')
             short_id = url_object.short_id
             hits = url_object.hits
             short_url = request.host_url + short_id
+            
             return render_template('index.html', data=[short_url, hits])
+
+
+        # create a hashed url with custom url
+        elif short_id:
+            new_link = ShortUrls(original_url=url, short_id=short_id, created_at=datetime.now())
+            data = manage.add_newurl(new_link)
+
+            return render_template('index.html', data=data)
+
 
         # generate a url hash
         elif not short_id:
@@ -36,22 +51,8 @@ def index():
 
         new_link = ShortUrls(
             original_url=url, short_id=short_id, created_at=datetime.now())
-        db.session.add(new_link)
-        db.session.commit()
-        short_url = request.host_url + short_id
-
-        return render_template('index.html', short_url=short_url)
-
-    # if request.method == 'GET':
-    #     # short_id = request.form.get('short_url')
-    #     # link = ShortUrls.query.filter_by(short_id=short_id).first()
-    #     link = None
-    #     if link:
-    #         return render_template('index.html', hits=link.hits, short_url=link.short_id, original_url=link.original_url)
-        
-    #     else:
-    #         flash('Invalid URL')
-    #         return redirect(url_for('index'))
+        data = manage.add_newurl(new_link)
+        return render_template('index.html', data=data)
 
     return render_template('index.html')
 
